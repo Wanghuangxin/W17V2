45c091a4976b65dab64281967087145413241f41
WHX <1114994246@qq.com>
2026-06-30 19:54:22 +0800

    Fix unlock combo row and column definitions

--- w17v2/keymaps/vial/config.h
+++ w17v2/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x25, 0xB1, 0x80, 0xE7, 0x7D, 0x27, 0x4A, 0xEF}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
